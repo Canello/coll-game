0a52864c02124379bbb0975afb1e1c7400f73425
Gustavo Canello <gustavobrandicanello@gmail.com>
2024-05-09 20:32:47 -0300

    refactor: refactored wall collision strategy for clarity

--- src/layers/game/collision_strategy.py
+++ src/layers/game/collision_strategy.py
@@ -28,39 +28,46 @@ class WallCollisionStrategy(CollisionStrategy):
         super().__init__(owner)
 
     def apply(self, obj: WorldObject, on_collision: Callable[[], Any]=lambda: None) -> None:
-        # in the x axis, we have four sides in total, from both objects
-        # two sides delimit the x intersection, the two in the middle
+        normalized_penetration_x = self._get_normalized_penetration_x(obj)
+        normalized_penetration_y = self._get_normalized_penetration_y(obj)
+        if normalized_penetration_y > normalized_penetration_x:
+            self._move_outwards_to_closest_x_border(obj)
+        else:
+            self._move_outwards_to_closest_y_border(obj)
+
+    def _get_normalized_penetration_x(self, obj: WorldObject) -> float:
+        # in the x axis, we have four sides in total from both objects
+        # two of them delimit the x intersection between the objects - the two sides in the middle
         sides_x = [self._owner.left, self._owner.right, obj.left, obj.right]
         sides_x.sort()
         penetration_x = sides_x[2] - sides_x[1]
         normalized_penetration_x = penetration_x / obj.width
-
-        # same logic from above, but for y axis
+        return normalized_penetration_x
+    
+    def _get_normalized_penetration_y(self, obj: WorldObject) -> float:
+        # in the y axis, we have four sides in total from both objects
+        # two of them delimit the y intersection between the objects - the two sides in the middle
         sides_y = [self._owner.top, self._owner.bottom, obj.top, obj.bottom]
         sides_y.sort()
         penetration_y = sides_y[2] - sides_y[1]
         normalized_penetration_y = penetration_y / obj.height
-
-        if normalized_penetration_y > normalized_penetration_x:
-            # move outwards in the x direction to the closest border from the center
-            distance_to_left_border = abs(obj.center.x - self._owner.left)
-            distance_to_right_border = abs(obj.center.x - self._owner.right)
-            if distance_to_left_border < distance_to_right_border:
-                # set obj x to left border - width
-                obj.x = self._owner.left - obj.width
-            else:
-                # set obj x to right border
-                obj.x = self._owner.right
+        return normalized_penetration_y
+    
+    def _move_outwards_to_closest_x_border(self, obj: WorldObject) -> None:
+        distance_to_left_border = abs(obj.center.x - self._owner.left)
+        distance_to_right_border = abs(obj.center.x - self._owner.right)
+        if distance_to_left_border < distance_to_right_border:
+            obj.x = self._owner.left - obj.width
         else:
-            # move outwards in the y direction to the closest border from the center
-            distance_to_top_border = abs(obj.center.y - self._owner.top)
-            distance_to_bottom_border = abs(obj.center.y - self._owner.bottom)
-            if distance_to_top_border < distance_to_bottom_border:
-                # set obj y to top border - height
-                obj.y = self._owner.top - obj.height
-            else:
-                # set obj y to bottom border
-                obj.y = self._owner.bottom
+            obj.x = self._owner.right
+
+    def _move_outwards_to_closest_y_border(self, obj: WorldObject) -> None:
+        distance_to_top_border = abs(obj.center.y - self._owner.top)
+        distance_to_bottom_border = abs(obj.center.y - self._owner.bottom)
+        if distance_to_top_border < distance_to_bottom_border:
+            obj.y = self._owner.top - obj.height
+        else:
+            obj.y = self._owner.bottom
 
 class GhostCollisionStrategy(CommonCollisionStrategy):
     def __init__(self, owner: WorldObject) -> None:
