e3c4b41c9f419402a45deef44aa0b499c600b60f
Gustavo Canello <gustavobrandicanello@gmail.com>
2024-05-09 21:13:42 -0300

    fix: fix type annotation and error message

--- src/errors.py
+++ src/errors.py
@@ -1,4 +1,4 @@
 class EventNotFoundError(Exception):
-    def __init__(self, event_type: str):
-        self.message = "There are no listeners for the event type: " + event_type
+    def __init__(self, event_type: str) -> None:
+        self.message = "There are no listeners for event type: " + event_type
         super().__init__(self.message)
