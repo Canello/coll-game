c935cb36e16bc96b787c6324072115c425dec4d8
Gustavo Canello <gustavobrandicanello@gmail.com>
2024-05-11 22:03:51 -0300

    build: add some object to world

--- src/layers/game/world_builder.py
+++ src/layers/game/world_builder.py
@@ -1,6 +1,9 @@
 from .world import World
 from .game_factory import GameFactory
 from .objects.player import Player
+from .objects.ball import Ball
+from .objects.wall import Wall
+from .objects.goal import Goal
 
 class WorldBuilder:
     def __init__(self) -> None:
@@ -9,7 +12,9 @@ class WorldBuilder:
     def build(self) -> World:
         # create objects and set their positions
         player = Player()
-        objects = []
+        objects = [
+            Wall(x=40, y=160),
+        ]
 
         # instantiate world
         world = GameFactory.make_world(player)
